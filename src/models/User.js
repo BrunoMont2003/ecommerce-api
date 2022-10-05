1197064ac35320b0c6b9d0e91981e8bba34d0793
brunomont <brunoadrian.mont@gmail.com>
2022-10-04 19:45:35 -0500

    feat: user model ✨

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose'
+
+const UserSchema = new Schema({
+  role: String,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  shippingAddress: {
+    country: String,
+    city: String,
+    province: String,
+    street: String,
+    zip: String
+  }
+})
+
+export default model('User', UserSchema)
