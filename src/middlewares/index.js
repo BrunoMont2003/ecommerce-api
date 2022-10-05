e78ecf1a80492ee468cdd84f9b4fb77dd090e385
brunomont <brunoadrian.mont@gmail.com>
2022-10-05 15:50:27 -0500

    feat: add product exists validator middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,12 +1,16 @@
 import CreateUserValidatorMiddleware from './validator/auth/create-user.validator.middleware.js'
-import CreateProductValidatorMiddleware from './validator/create-product.validator.middleware.js'
+import CreateProductValidatorMiddleware from './validator/products/create-product.validator.middleware.js'
+import UpdateProductValidatorMiddleware from './validator/products/update-product.validator.middleware.js'
 import LoginValidatorMiddleware from './validator/auth/login.validator.middleware.js'
 import AuthValidatorMiddleware from './validator/auth/auth.validator.middleware.js'
 import AdminValidatorMiddleware from './validator/auth/admin.validator.middleware.js'
+import ProductExistsMiddleware from './validator/products/product-exists.validator.middleware.js'
 export {
   CreateUserValidatorMiddleware,
   CreateProductValidatorMiddleware,
+  UpdateProductValidatorMiddleware,
   LoginValidatorMiddleware,
   AuthValidatorMiddleware,
-  AdminValidatorMiddleware
+  AdminValidatorMiddleware,
+  ProductExistsMiddleware
 }
---
+++ src/middlewares/validator/products/product-exists.validator.middleware.js
@@ -0,0 +1,17 @@
+import Product from '../../../models/Product.js'
+
+const productExistsValidator = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    if (!product) return res.status(404).json({ message: 'Product not found' })
+    next()
+  } catch (error) {
+    res.status(400).json({
+      message: 'Something went wrong',
+      error
+    })
+  }
+}
+
+export default productExistsValidator
